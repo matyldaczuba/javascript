357644ba19e8b8c0e8f6bb46afd20b9098921f5c
Matylda Czuba <matylda.czuba@pm.me>
2018-10-06 16:11:10 +0200

    operatory

---
+++ js/9-operatory.js
@@ -0,0 +1,11 @@
+let wartosc = (2+2 > 4-1);
+
+wartosc = !(10 + 10 > 100); // ! to zaprzeczenie, w nawiasie wyjdzie false ale przez zaprzeczenie bedzie true
+
+wartosc = 50 === "50"; // == porownuje wartosc a === porownuje wartosc i typ
+
+wartosc =(2 + 2 === 4 &&  3 + 1 === 4);
+
+wartosc = (2 + 2 === 4) ? "Równa się 4" : "Nie równa się 4"; //turner operator - zamiast if ... else ... 
+
+console.log(wartosc);
