133793f7e7a3af22042ee14352007e932f4e67e8
Matylda Czuba <matylda.czuba@pm.me>
2018-10-06 16:59:47 +0200

    math

--- js/8-object.math.js
+++ js/8-object.math.js
@@ -3,7 +3,7 @@ let randomNumber = Math.round(Math.random() * 10);
 console.log(randomNumber);
 
 //modulo
-    console.log("Wynik z modulo" + randomNumber % 5);
+    console.log("Wynik z modulo" + randomNumber % 2);
 
 if (randomNumber % 5 == 0) {
     console.log("Liczba parzysta");
