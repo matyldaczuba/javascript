1dea1caba70056826fc34a7fc10a3cbda9a68d45
Matylda Czuba <matylda.czuba@pm.me>
2018-10-06 11:26:18 +0200

    funkcja strzałkowa

--- js/3-functions.js
+++ js/3-functions.js
@@ -16,6 +16,7 @@ let wynik = wyswietlImie("Tomek");
 
 console.log(wynik);*/
 
+/*FUNCTION ES5
 
 function dodawanie(liczba1, liczba2, liczba3){
     let result = liczba1 + liczba2 + liczba3;
@@ -24,3 +25,21 @@ function dodawanie(liczba1, liczba2, liczba3){
 
 let sum = dodawanie(1,2,3);
 console.log(sum);
+
+*/
+
+
+//FUNCTION ES6
+//jeden parametr
+/*
+let wyswietlImie = imie => imie;
+
+console.log(wyswietlImie("Ala"));*/
+
+//wiecej niz 1 parametr
+
+let wyswietlImie = (imie, imie2) => "Imie : " + imie + " Imie2: " + imie2;
+
+console.log(wyswietlImie("Ala", "Karol"));
+
+
