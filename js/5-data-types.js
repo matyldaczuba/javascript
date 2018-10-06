08abd449bde2f2807a145345d2342b3a45e5cd3a
Matylda Czuba <matylda.czuba@pm.me>
2018-10-06 12:35:20 +0200

    typy zmiennych

---
+++ js/5-data-types.js
@@ -0,0 +1,32 @@
+let liczba = 50;
+
+let none = null;
+
+let tekst = "text - string"
+
+let trueFalse = true;
+
+let tablica = [];
+
+let object = {
+    klucz: "wartość", //tablica[0] --> 1
+    klucz2: "wartość2" // object['klucz'] lub object.klucz --> wartość
+};
+
+console.log(object); 
+
+
+// ES5
+
+let number = 10;
+let zdanie = "Wyświetl " + number + " i zakończ zadanie";
+
+console.log(zdanie);
+
+
+// ES6
+
+let number2 = 10;
+let zdanie2 = `Wyświetl ${number2} i zakończ zadanie`;
+
+console.log(zdanie2);
