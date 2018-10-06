1fcb47fbee157fabade91259dadaf6d6eedcf03d
Matylda Czuba <matylda.czuba@pm.me>
2018-10-06 10:06:56 +0200

    zmienne

---
+++ js/2-variables.js
@@ -0,0 +1,18 @@
+console.log("działa");
+
+let imie;
+let imieMeskie = "Kacper";
+
+imie = "Monika";
+
+imie = "Gosia"
+
+imie = imieMeskie;
+
+let liczba = 5000;
+
+const innaLiczba = 1000;
+
+console.log(imie);
+
+console.log(liczba);
