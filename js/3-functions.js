94972f38c083101cd234ee1e752c0ffda5ba8609
Matylda Czuba <matylda.czuba@pm.me>
2018-10-06 10:48:04 +0200

    funkcja

---
+++ js/3-functions.js
@@ -0,0 +1,17 @@
+/*function wyswietlImie(imie){
+    console.log(imie);
+    
+}
+
+wyswietlImie("Tomek");*/
+
+
+let wyswietlImie = function(imie) {
+    return imie;
+    
+}
+
+
+let wynik = wyswietlImie("Tomek");
+
+console.log(wynik);
